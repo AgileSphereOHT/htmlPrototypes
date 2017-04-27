e4d30abf38fdf5154be0499cc8c074c8b527589f
David Smith <dave@javabeat.co.uk>
2017-04-27 11:34:16 +0100

    Update service title

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Overseas Healthcare Team',
+  serviceName: 'Overseas Healthcare',
 
   // Default port that prototype runs on
   port: '3000',
